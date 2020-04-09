5afdfdd69f9b789ba7da11a1a00fba73700c562d
EvanLavic <airgamer22@gmail.com>
2020-04-09 13:16:41 +0300

    Add format +x (xxx) xxxxxxx, fix bug with left char

--- lab 4.py
+++ lab 4.py
@@ -4,11 +4,12 @@ f = open("Telnum.txt", "r")
 lines = f.read().splitlines()
 f.close()
 answer = []
-lc = 2
 for i in lines:
+    lc = 2
     if len(i) > 12:
         lc = len(i) - 10
-    answer.append("+1" + i[lc:])
+    rc = lc + 3
+    answer.append("+1 (" + i[lc:rc] + ") " + i[rc:])
 
 fw = open ("Answer.txt", "w")
 fw.write(str (answer))
