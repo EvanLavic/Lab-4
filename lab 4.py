441e952f73b937926755f4225b3806199848efcd
EvanLavic <airgamer22@gmail.com>
2020-04-09 12:15:26 +0300

    commit on master

--- lab 4.py
+++ lab 4.py
@@ -1,5 +1,13 @@
 
 f = open("Telnum.txt", "r")
 
-lines = f.readlines()
+lines = f.read().splitlines()
 
+answer = []
+lc = 2
+for i in lines:
+    if len(i) > 12:
+        lc = 3
+    answer.append("+1" + i[lc:])
+
+print (answer)
